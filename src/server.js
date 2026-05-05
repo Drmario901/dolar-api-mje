275d324b60778ac93dc04f272443476f87f8163e
Edithson Maestracci <edithson@zohomail.com>
2026-05-05 17:02:04 -0400

    Changed request method to scrapping

--- src/server.js
+++ src/server.js
@@ -14,22 +14,22 @@ const fastify = Fastify({ logger: true })
 
 await fastify.register(cors, { origin: '*' })
 
-//const JWT_SECRET = process.env.JWT_SECRET
-// if (!JWT_SECRET) {
-//   throw new Error('jwt secret missing')
-// }
+const JWT_SECRET = process.env.JWT_SECRET
+if (!JWT_SECRET) {
+  throw new Error('jwt secret missing')
+}
 
-// await fastify.register(jwt, { secret: JWT_SECRET })
+await fastify.register(jwt, { secret: JWT_SECRET })
 
-// fastify.addHook('preHandler', async (request, reply) => {
-//   if (request.routerPath === '/health') return
+fastify.addHook('preHandler', async (request, reply) => {
+  if (request.routerPath === '/health') return
 
-//   try {
-//     await request.jwtVerify()
-//   } catch {
-//     return reply.code(401).send({ error: 'Unauthorized' })
-//   }
-//})
+  try {
+    await request.jwtVerify()
+  } catch {
+    return reply.code(401).send({ error: 'Unauthorized' })
+  }
+})
 
 await fastify.register(routes)
 
